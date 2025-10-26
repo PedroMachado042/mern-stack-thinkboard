b193cade240e06908020be1a22e0ca9bccbade3c
Pedro Machado <machado.trabalhos@gmail.com>
2025-10-26 13:23:16 -0300

    prepared for the deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios"
 
+// in production you cant call localhost, so the url is dynamic
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api":"/api";
 const api = axios.create({
-    baseURL: "http://localhost:5001/api"
+    baseURL: BASE_URL
 })
 
 export default api;
