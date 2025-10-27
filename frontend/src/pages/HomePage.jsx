c15a5cb592321938ea65a19801f80a8118143547
Pedro Machado <machado.trabalhos@gmail.com>
2025-10-27 10:33:29 -0300

    Update HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -39,9 +39,11 @@ const HomePage = () => {
       {isRateLimited && <RateLimitedUI />}
 
       <div className="max-w-7xl mx-auto p-4 mt-6">
-        {loading && <AxeIcon className="animate-spin size-10" /> }
+        {loading && <div className="min-h-screen bg-base-200 flex items-center justify-center">
+          <AxeIcon className="animate-spin size-10" />
+        </div> }
 
-        {notes.length === 0 && !loading && !isRateLimited && <NotesNotFound />}
+        {notes.length === 0 && !loading &&!isRateLimited && <NotesNotFound />}
 
         {notes.length > 0 && !isRateLimited && (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
