9b01009452b329b349227965d870dfe46424d6be
Pedro Machado <machado.trabalhos@gmail.com>
2025-10-27 10:30:27 -0300

    Update HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -39,9 +39,9 @@ const HomePage = () => {
       {isRateLimited && <RateLimitedUI />}
 
       <div className="max-w-7xl mx-auto p-4 mt-6">
-        {loading && <div className="text-center text-primary py-10">Loading notes...</div> }
+        {loading && <AxeIcon className="animate-spin size-10" /> }
 
-        {notes.length === 0 && !loading &&!isRateLimited && <NotesNotFound />}
+        {notes.length === 0 && !loading && !isRateLimited && <NotesNotFound />}
 
         {notes.length > 0 && !isRateLimited && (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
